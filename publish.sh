9b7085bbe8ae7c573a7e2f4a236766c8535e785b
bozdoz <howaboutben@gmail.com>
2017-01-19 00:26:20 -0400

    update to publish script;

--- publish.sh
+++ publish.sh
@@ -4,6 +4,13 @@ VERSION=$(node --eval "console.log(require('./package.json').version);")
 
 # publish master branch
 git commit -am "v$VERSION"
-git tag v$VERSION -f
+
+if [[ $1 ]]; then
+	# first arg is comment
+	git tag -a v$VERSION -m "$1" -f
+else
+	git tag v$VERSION -f
+fi
+	
 git push --tags -f
 git push
