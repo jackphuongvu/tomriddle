5097182991ab6a8c0e7133d75f3c1c1439785fbf
bozdoz <howaboutben@gmail.com>
2021-01-27 02:22:56 -0400

    adds preloaded audio to try to get audio syncing

--- src/utils/MultiAudio.js
+++ src/utils/MultiAudio.js
@@ -1,16 +1,20 @@
-function MultiAudio(src, instances) {
+// I don't know why these are here
+// https://stackoverflow.com/questions/9811429/html5-audio-tag-on-safari-has-a-delay
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+
+function MultiAudio(src, instances = 5) {
   const output = [];
-  const num = instances || 5;
   const Audio = window.Audio || function Audio() {};
 
-  for (let i = 0; i < num; i += 1) {
+  for (let i = 0; i < instances; i += 1) {
     output.push(new Audio(src));
   }
 
   let current = 0;
 
   this.play = function play() {
-    const audio = output[(current += 1) % num];
+    const audio = output[(current += 1) % instances];
     audio.currentTime = 0;
     audio.play();
   };
