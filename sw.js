0537e1a325e54807740a8d4237803a12c6ae18f3
bozdoz <howaboutben@gmail.com>
2021-01-29 00:18:23 -0400

    try removing arrow functions?

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-restricted-globals */
 const CACHE_NAME = 'typewritesomething-v1.0.0';
 
-/* eslint-disable no-restricted-globals */
-self.addEventListener('install', (e) => {
+self.addEventListener('install', function install(e) {
   e.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) =>
-        cache.addAll([
+    caches.open(CACHE_NAME).then(function caching(cache) {
+      console.log('opened cache');
+
+      return cache
+        .addAll([
           '/',
           '/index.html',
           '/dist/',
@@ -20,12 +22,29 @@ self.addEventListener('install', (e) => {
           '/static/fonts/specialelite-webfont.woff',
           '/static/fonts/specialelite-webfont.woff2',
         ])
-      )
+        .then(function cached() {
+          console.log('finished caching?');
+        });
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', function handleFetch(e) {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches
+      .match(e.request)
+      .then(function getCache(response) {
+        if (response) {
+          console.error('cached');
+          return response;
+        }
+        console.error('NOT cached');
+        return fetch(e.request);
+      })
+      .catch(function fail(err) {
+        console.error(err);
+
+        return fetch(e.request);
+      })
   );
 });
