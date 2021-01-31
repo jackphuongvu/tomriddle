49d0bf8432727cf59529a5baff3342a4cec629eb
bozdoz <howaboutben@gmail.com>
2021-01-31 13:32:23 -0400

    adds click to start

--- src/index.js
+++ src/index.js
@@ -7,19 +7,39 @@ import App from './App';
 import './tracking/analytics';
 import './tracking/sentry';
 
-const splashAnimTime = 1500;
+const startApp = (e) => {
+  // wait for splash transition
+  const splashAnimTime = 300;
+  const splash = document.getElementById('splash');
 
-/**
- * basic app handlers
- */
-window.addEventListener('load', () => {
+  if (e.altKey || e.ctrlKey || e.metaKey) {
+    // user might be trying to do something else
+    return;
+  }
+
+  document.body.removeEventListener('click', startApp);
+  document.body.removeEventListener('keydown', startApp);
+
+  setTimeout(App.start.bind(App), splashAnimTime);
+
+  splash.classList.add('hide');
+};
+
+const onload = () => {
   if (window.location.hash) {
     window.location.hash = '';
   }
 
-  // wait for splash transition
-  setTimeout(App.start.bind(App), splashAnimTime);
-});
+  document.body.addEventListener('click', startApp);
+  document.body.addEventListener('keydown', startApp);
+
+  window.removeEventListener('load', onload);
+};
+
+/**
+ * basic app handlers
+ */
+window.addEventListener('load', onload);
 
 // Register service worker to control making site work offline
 
