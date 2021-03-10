99b3fc4f65f6bf262bf2cf928bbf07e3b4b7a62f
bozdoz <howaboutben@gmail.com>
2021-03-10 01:32:46 -0400

    better types for createElement

--- src/utils/createElement.ts
+++ src/utils/createElement.ts
@@ -1,16 +1,16 @@
-const createElement = (
-  tagname = 'div',
-  params: Partial<Writable<HTMLElement>>
-): HTMLElement => {
+function createElement<T extends keyof HTMLElementTagNameMap>(
+  tagname: T,
+  params?: Partial<Writable<HTMLElementTagNameMap[T]>>
+): HTMLElementTagNameMap[T] {
   const elem = document.createElement(tagname);
 
   // eslint-disable-next-line guard-for-in
   for (const key in params) {
-    const typedKey = key as WritableKeys<HTMLElement>;
+    const typedKey = key as keyof typeof params;
     (elem as any)[typedKey] = params[typedKey];
   }
 
   return elem;
-};
+}
 
 export default createElement;
